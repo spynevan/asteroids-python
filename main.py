5084a36c7121667c33bcf6f15918faeed4247445
spynevan <spynevan007@gmail.com>
2025-11-16 14:38:18 -0600

    added character sprite to screen

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
 from logger import log_state
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS 
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    delta_time = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
